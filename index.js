7cb6bf1be8459aaf199f9335996d7f9b5d546773
Qurypto musa <musaquam7@gmail.com>
2023-11-21 06:40:08 +0000

    demo on how to create GraphQL query to interact with the Apollo Server

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
+// Import Apollo Server and schema 
 const { ApolloServer } = require("apollo-server");
 const { importSchema } = require("graphql-import");
-const EtherDataSource = require("./datasource/ethDatasource");
-const typeDefs = importSchema("./schema.graphql");
 
+// Import custom data source
+const EtherDataSource = require("./datasource/ethDatasource");  
+
+// Import GraphQL schema
+const typeDefs = importSchema("./schema.graphql"); 
+
+// Require environment variables
 require("dotenv").config();
 
+// Define GraphQL resolvers
 const resolvers = {
   Query: {
-    getEthByAddress: (root, _args, { dataSources }) =>
+    // Get ether balance for an address
+    etherBalanceByAddress: (root, _args, { dataSources }) =>  
       dataSources.ethDataSource.etherBalanceByAddress(),
-    getTotalSupplyEth: (root, _args, { dataSources }) =>
+
+    // Get total ether supply
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
       dataSources.ethDataSource.totalSupplyOfEther(),
-    //Paste Code for New Resolver Functions
+
+    // Get latest Ethereum price
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    // Get average Ethereum block confirmation time
+    blockConfirmationTime: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getBlockConfirmationTime(),
   },
 };
 
+// Create Apollo Server instance
 const server = new ApolloServer({
+  
+  // Pass in schema and resolvers
   typeDefs,
   resolvers,
+  
+  // Set up data sources
   dataSources: () => ({
-    ethDataSource: new EtherDataSource(),
+    ethDataSource: new EtherDataSource(), 
   }),
 });
 
+// Set timeout and start server 
 server.timeout = 0;
 server.listen("9000").then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
